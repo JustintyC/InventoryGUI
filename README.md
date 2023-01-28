6336626fcc3afb4668a9168aec48f70e5fbe469f
JustinC <tcheun07@students.cs.ubc.ca>
2023-01-28 14:44:36 -0800

    Added: - Description

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # **Gaming Inventory GUI**
 
 ## Description
+This application provides a GUI that can potentially be 
+implemented into a game that requires an inventory storage 
+system. As someone who has plenty of experience in video games,
+their development has given me great interest. This project 
+will aim to provide me with experience in the development of
+GUI, as well as potentially providing others with inspiration
+on how to develop their own inventory system.
 
 ## User Stories
 ### As a user, I want to be able to:
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,10 +1,10 @@
 package ui;
 
 public class Main {
-    //public static void main(String[] args) {
-    //    for (int i = 0; i <= 100; i++) {
-    //        System.out.println("AHAHAHAHAHAH CPSC110 PSET9 AHAHAH NATURAL RECURSION AHAHAHAHAHAHAHAHAH CPSC");
-    //        System.out.println("AHAHAHAHAHAHAHAHAHAHAHAHAHAH TRUST THE NATURAL AHAHAH TRUST THE NATURAL RECURSION");
-    //    }
-    //}
+    public static void main(String[] args) {
+        for (int i = 0; i <= 60; i++) {
+            System.out.println("AHAHAHAHAHAH CPSC110 PSET9 AHAHAH NATURAL RECURSION AHAHAHAHAHAHAHAHAH CPSC");
+            System.out.println("AHAHAHAHAHAHAHAHAHAHAHAHAHAH TRUST THE NATURAL AHAHAH TRUST THE NATURAL RECURSION");
+        }
+    }
 }
